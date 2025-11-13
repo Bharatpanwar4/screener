49b0dc6a19b4e6f893ef838dd7eabd784e54e563
bharat <bharatpanwar4@gmail.com>
2025-11-13 23:18:16 +0530

    implemented header ui with list

---
+++ src/common/Header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import NavItems from "./NavItems";
+import NavDropdown from "./NavDropdown";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href={"/"}>
+          <Image
+            src={"/assets/icons/logo.svg"}
+            alt="screener logo"
+            height={32}
+            width={140}
+            className="h-8 w-auto cursor-pointer"
+          />
+        </Link>
+        {/* navItems */}
+        <nav className="hidden sm:block ">
+          <NavItems />
+        </nav>
+        {/* user dropdown */}
+        <NavDropdown />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
